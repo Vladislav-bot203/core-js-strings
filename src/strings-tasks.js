3bda83ed57628cf7242610c34f5bbe251ced8737
Vladislav-bot203 <70837097+Vladislav-bot203@users.noreply.github.com>
2024-11-11 19:24:14 +0300

    feat: implemented orderAlphabetically function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -104,8 +104,9 @@ function reverseString(str) {
  *   orderAlphabetically('textbook') => 'bekoottx'
  *   orderAlphabetically('abc123xyz') => '123abcxyz'
  */
-function orderAlphabetically(/* str */) {
-  throw new Error('Not implemented');
+function orderAlphabetically(str) {
+  const arrToSort = str.split('');
+  return arrToSort.sort().join('');
 }
 
 /**
